cbe7e766c2124ef02a50705efdf704331fee573c
Serpal64 <serpallop64@gmail.com>
2026-05-22 22:27:30 +0200

    feat: implemented a button class for generalizing image buttons and text based buttons

---
+++ src/AppButton.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class AppButton extends JButton{
+    
+    // Quitar estilo feo del java swing normal
+    private void removeStyle(){
+
+        setBorderPainted(false);
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+    }
+
+    // Constructor para botones con sólo imágenes o iconos
+    public AppButton(ImageIcon icon, int width, int height){
+
+        super();
+        Image scaled = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        setIcon(new ImageIcon(scaled));
+        removeStyle();
+
+    }
+
+    // Constructor para botones normales con texto
+    public AppButton(String txt, String back_color, String font_color){
+
+        super(txt);
+        removeStyle();
+        setBackground(Color.decode(back_color));
+        setForeground(Color.decode(font_color));
+
+    }
+
+}
--- src/Home.java
+++ src/Home.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -10,13 +12,21 @@ public class Home {
         JFrame jf = new JFrame("Home");
         BorderLayout bl = new BorderLayout(5, 5);
 
+        // Panel principal de la vista
         JPanel main_panel = new JPanel(bl);
         main_panel.setBackground(Color.decode("#F3EDDF"));
 
-        JPanel header = new JPanel();
+        // El navbar de arriba
+        JPanel header = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 20));
         header.setPreferredSize(new Dimension(390, 85));
         header.setBackground(Color.decode("#E73331")); 
         main_panel.add(header, BorderLayout.NORTH);
+
+        // Iconos del navbar
+        ImageIcon img = new ImageIcon(Home.class.getResource("images/menu.png"));
+        AppButton menu = new AppButton(img, img.getIconWidth(), img.getIconHeight());
+
+        header.add(menu);
 
         jf.add(main_panel);
         jf.setSize(390, 844);
