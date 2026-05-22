a1c0c42b1de05ab1619aee843944febc0245fdca
Serpal64 <serpallop64@gmail.com>
2026-05-22 23:04:00 +0200

    feat: added navbar but needs polishing

--- src/Home.java
+++ src/Home.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Home {
@@ -17,16 +18,41 @@ public class Home {
         main_panel.setBackground(Color.decode("#F3EDDF"));
 
         // El navbar de arriba
-        JPanel header = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 20));
+        JPanel header = new JPanel(new BorderLayout(5, 5));
         header.setPreferredSize(new Dimension(390, 85));
         header.setBackground(Color.decode("#E73331")); 
+
+        // Para poner los botones que están a la izquierda y derecha
+        JPanel header_left = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        header_left.setBackground(Color.decode("#E73331"));
+
+        ImageIcon img_menu = new ImageIcon(Home.class.getResource("images/menu.png"));
+        AppButton menu = new AppButton(img_menu, img_menu.getIconWidth(), img_menu.getIconHeight());
+        ImageIcon img_search = new ImageIcon(Home.class.getResource("images/search_bar.png"));
+        JLabel search = new JLabel(img_search);
+
+        header_left.add(menu);
+        header_left.add(search);
+
+
+        JPanel header_right = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        header_right.setBackground(Color.decode("#E73331"));
+
+        ImageIcon img_home = new ImageIcon(Home.class.getResource("images/home.png"));
+        AppButton home = new AppButton(img_home, img_home.getIconWidth(), img_home.getIconHeight());
+        ImageIcon img_tomato = new ImageIcon(Home.class.getResource("images/tomato.png"));
+        AppButton tomato = new AppButton(img_tomato, img_tomato.getIconWidth(), img_tomato.getIconHeight());
+        ImageIcon img_basket = new ImageIcon(Home.class.getResource("images/basket.png"));
+        AppButton basket = new AppButton(img_basket, img_basket.getIconWidth(), img_basket.getIconHeight());
+
+        header_right.add(home);
+        header_right.add(tomato);
+        header_right.add(basket);
+
+        header.add(header_left, BorderLayout.WEST);
+        header.add(header_right, BorderLayout.EAST);
+
         main_panel.add(header, BorderLayout.NORTH);
-
-        // Iconos del navbar
-        ImageIcon img = new ImageIcon(Home.class.getResource("images/menu.png"));
-        AppButton menu = new AppButton(img, img.getIconWidth(), img.getIconHeight());
-
-        header.add(menu);
 
         jf.add(main_panel);
         jf.setSize(390, 844);
