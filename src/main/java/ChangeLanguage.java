452983e1d73623b16eeac3c9276466119abc4da8
Serpal64 <serpallop64@gmail.com>
2026-05-24 20:05:58 +0200

    feat: added language functionality with bundles

---
+++ src/main/java/ChangeLanguage.java
@@ -0,0 +1,57 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+
+public class ChangeLanguage implements ActionListener{
+
+    private final Locale[] locales;
+    private int currentLocaleIndex;
+    private final List<JComponent> components;
+    private final List<String> keys;
+
+    public ChangeLanguage(){
+
+        this.locales = new Locale[]{new Locale("es", "ES"), new Locale("en", "GB")};
+        this.components = new ArrayList<>();
+        this.keys = new ArrayList<>();
+        this.currentLocaleIndex = 0;
+
+    }
+
+    public void addComponent(JComponent comp, String key){
+        components.add(comp);
+        keys.add(key);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+        currentLocaleIndex = (currentLocaleIndex + 1) % locales.length;
+        Locale nextLocale = locales[currentLocaleIndex];
+        ResourceBundle bundle_text = ResourceBundle.getBundle("bundle.Bundle", nextLocale);
+
+        for(int i=0; i<components.size(); i++){
+            
+            if(components.get(i) instanceof JLabel jLabel){
+                jLabel.setText(bundle_text.getString(keys.get(i)));
+            }
+            else if(components.get(i) instanceof JButton jButton) {
+                jButton.setText(bundle_text.getString(keys.get(i)));
+            }
+            else if(components.get(i) instanceof JMenuItem jMItem){
+                jMItem.setText(bundle_text.getString(keys.get(i)));
+            }
+
+        }
+    }
+
+
+}
--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -1,11 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -14,32 +17,51 @@ public class Home {
     public static void main(String[] args) throws Exception {
 
         JFrame jf = new JFrame("Home");
+
         BorderLayout bl = new BorderLayout(5, 5);
 
         // Panel principal de la vista
         JPanel main_panel = new JPanel(bl);
         main_panel.setBackground(Color.decode("#F3EDDF"));
 
+        // Para cambiar el idioma
+        ChangeLanguage language_changer = new ChangeLanguage();
+
         // El navbar de arriba
         JPanel header = new JPanel(new MigLayout("insets 20 20 5 15, fillx, gap 0", "[][grow][]", "[center]"));
         header.setPreferredSize(new Dimension(390, 85));
         header.setBackground(Color.decode("#E73331"));
 
-        // Botón menú
-        ImageIcon img_menu = new ImageIcon(Home.class.getResource("images/menu.png"));
+        // Botón menú desplegable
+        ImageIcon img_menu = new ImageIcon(Home.class.getResource("images/header/menu.png"));
         AppButton menu = new AppButton(img_menu, img_menu.getIconWidth(), img_menu.getIconHeight());
 
-        // Buscador (ocupa toda la columna central)
-        ImageIcon img_search = new ImageIcon(Home.class.getResource("images/search_bar.png"));
+        JPopupMenu popupMenu = new JPopupMenu();
+
+        JMenuItem contact = new JMenuItem();
+        language_changer.addComponent(contact, "Contacto");
+        JMenuItem language = new JMenuItem();
+        language_changer.addComponent(language, "Idioma");
+
+        popupMenu.add(contact);
+        popupMenu.add(language);
+
+        // Abrir el menú al hacer clic en el botón
+        menu.addActionListener(e -> {
+            popupMenu.show(menu, 0, menu.getHeight()); // aparece justo debajo del botón
+        });
+
+        // Etiqueta de buscador (meramente estético)
+        ImageIcon img_search = new ImageIcon(Home.class.getResource("images/header/search_bar.png"));
         JLabel search = new JLabel(img_search);
 
-        // Panel derecho con los 3 iconos juntos
+        // Panel derecho con icono home, productos y carrito
         JPanel header_right = new JPanel(new MigLayout("insets 0, gap 5", "[][][]", "[center]"));
         header_right.setBackground(Color.decode("#E73331"));
 
-        ImageIcon img_home   = new ImageIcon(Home.class.getResource("images/home.png"));
-        ImageIcon img_tomato = new ImageIcon(Home.class.getResource("images/tomato.png"));
-        ImageIcon img_basket = new ImageIcon(Home.class.getResource("images/basket.png"));
+        ImageIcon img_home   = new ImageIcon(Home.class.getResource("images/header/home.png"));
+        ImageIcon img_tomato = new ImageIcon(Home.class.getResource("images/header/tomato.png"));
+        ImageIcon img_basket = new ImageIcon(Home.class.getResource("images/header/basket.png"));
 
         AppButton home   = new AppButton(img_home,   img_home.getIconWidth(),   img_home.getIconHeight());
         AppButton tomato = new AppButton(img_tomato, img_tomato.getIconWidth(), img_tomato.getIconHeight());
@@ -49,7 +71,6 @@ public class Home {
         header_right.add(tomato);
         header_right.add(basket);
 
-        // Añadir al header
         header.add(menu,         "");
         header.add(search,       "growx, gapright 0");   // se estira para ocupar el centro
         header.add(header_right, "");
@@ -61,5 +82,8 @@ public class Home {
         jf.setResizable(false);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setVisible(true);    
+
+        // Para inicializar los idiomas
+        language_changer.actionPerformed(new ActionEvent(basket, 0, null));
     }
 }
